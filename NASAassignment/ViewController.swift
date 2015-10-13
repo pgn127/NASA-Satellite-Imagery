b778f3ed963192c088571cad6e06bf0c2b1d6ca3
Pamela Needle <Pamneedle@pamelas-macbook-pro.local>
2015-10-13 12:13:55 +1100

    Initial Commit

---
+++ NASAassignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NASAassignment
+//
+//  Created by Pamela Needle on 10/13/15.
+//  Copyright © 2015 Pamela Needle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
