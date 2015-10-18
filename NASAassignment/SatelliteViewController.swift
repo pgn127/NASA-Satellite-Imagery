77fe149c2c63f04dbdf8cfe3710290ee285a0327
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-10-18 15:51:47 +1100

    need to do loading indicator and fix do catch error

--- NASAassignment/SatelliteViewController.swift
+++ NASAassignment/SatelliteViewController.swift
@@ -16,20 +16,35 @@ class SatelliteViewController: UIViewController {
     @IBOutlet weak var datelabel: UILabel!
     var longitude: String?
     var latitude: String?
-    var imageurl: String?
-    var datestring: String?
+    //var imageurl: String?
+    //var datestring: String?
+    var counter: Int = 7
+    var loadingComplete: Bool = false
+    var locHistory = [locInfo?](count: 7, repeatedValue: nil)
+    var locIndex: Int?
     var cloudscore = true
     let baseURL = "https://api.nasa.gov/planetary/earth/imagery"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       // print("ran request")
-        //var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: baseURL))
-        self.longitude = "100.5233"
-        self.latitude = "13.7367"
-                
+//        var actInd : UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0,0, 50, 50)) as UIActivityIndicatorView
+//        actInd.center = self.view.center
+//        actInd.hidesWhenStopped = true
+//        actInd.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+//        view.addSubview(actInd)
+//        actInd.startAnimating()
+//        actInd.stopAnimating()
+//        actInd.removeFromSuperview()
+       
+        self.longitude = "-0.1276250"
+        self.latitude = "51.5033630"
+
         performNASARequestSequence(longitude!, latitude: latitude!)
-//        
+//        if loadingComplete{
+//        var timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("displayImages"), userInfo: nil, repeats: true)
+//        }
+
+//
 //        var currentDate = NSDate()
 //        let dateFormatter = NSDateFormatter()
 //        dateFormatter.dateFormat = "YYYY-MM-dd"
@@ -60,39 +75,58 @@ class SatelliteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    struct locInfo {
+        
+        //tiledata properties
+        var pic : UIImage
+        var url : NSURL
+        var date: String
+        
+    }
+    
+    func displayImages(){
+        if locIndex == nil || locIndex == locHistory.count {
+            locIndex = 0
+        }
+        self.datelabel.text = locHistory[locIndex!]!.date
+        self.img.image = locHistory[locIndex!]!.pic
+        locIndex!++
+    }
+    
     func performNASARequestSequence(longitude: String, latitude: String){
         let currentDate = NSDate()
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "YYYY-MM-dd"
-        datestring = dateFormatter.stringFromDate(currentDate)
+        var datestring = dateFormatter.stringFromDate(currentDate)
         let cal = NSCalendar.currentCalendar()
         let periodcomponents = NSDateComponents()
-        for index in 1...7 {
+        var orderIndex = 6
+        for index in 0...6 {
 //            let cal = NSCalendar.currentCalendar()
 //            let periodcomponents = NSDateComponents()
             periodcomponents.month = -index
             let d = cal.dateByAddingComponents(periodcomponents, toDate: currentDate, options: [])
             if let date = d {
-                self.datestring = dateFormatter.stringFromDate(date)
-                print("date being looked up: \(self.datestring)")
+                datestring = dateFormatter.stringFromDate(date)
+                //print("date being looked up: \(datestring)")
             //self.datelabel.text = datestring
-                performNASARequest(longitude, latitude: latitude, date: self.datestring!)
+                performNASARequest(longitude, latitude: latitude, date: datestring, order: orderIndex)
+                orderIndex--
+                //let curloc = locInfo(pic: nil, url: nil, date: datestring)
+                //self.locHistory.insert(curloc, atIndex:0)
+                
             }
         }
     }
     
-    func performNASARequest(longitude: String, latitude: String, date: String) {
-//        var dateFormatter = NSDateFormatter()
-//        dateFormatter.dateFormat = "YYYY-MM-dd"
-        //let datestr = dateFormatter.stringFromDate(morningOfChristmas)
+    func performNASARequest(longitude: String, latitude: String, date: String, order: Int) {
         let urlComponents = NSURLComponents(string: baseURL)
         let lonQuery: NSURLQueryItem = NSURLQueryItem(name: "lon", value: longitude)
         let latQuery: NSURLQueryItem = NSURLQueryItem(name: "lat", value: latitude)
         let dateQuery: NSURLQueryItem = NSURLQueryItem(name: "date", value: date)
         let cloudQuery: NSURLQueryItem = NSURLQueryItem(name: "cloud_score", value: "True")
         let  keyQuery: NSURLQueryItem = NSURLQueryItem(name: "api_key", value: apikey)
-        //components.lat = latitude
-        //components.lon = longitude
+
         urlComponents!.queryItems = [lonQuery, latQuery, dateQuery, cloudQuery, keyQuery]
         
         let url = urlComponents?.URL
@@ -110,17 +144,18 @@ class SatelliteViewController: UIViewController {
                     let result = NSString(data: d, encoding:NSASCIIStringEncoding)!
                     //print("query result \(result)")
                     do{
+                        //print("current order \(order)")
                         let json = try NSJSONSerialization.JSONObjectWithData(d, options:NSJSONReadingOptions.AllowFragments )
+                        print("current order \(order)")
                         guard let dict : NSDictionary = json as? NSDictionary else {
                             print("not a dictionary")
                             return
                         }
                         if let date = dict["date"] as? String, img = dict["url"] as? String{
-                            self.datestring = date
-                            //self.datelabel.text = self.datestring!
+                            //print("date and image found for date \(date)")
                             //print("datestring after calling request: \(self.datestring)")
-                            self.imageurl = img
-                            self.fetchImage(img)
+                            //print("current order \(order)")
+                            self.fetchImage(img, withDate: date, currentCount: order)
                             //print("imageurl: \(self.imageurl)")
                             
                         }  else{
@@ -138,7 +173,7 @@ class SatelliteViewController: UIViewController {
     }
     
     
-    func fetchImage(urlString: String){
+    func fetchImage(urlString: String, withDate: String, currentCount: Int){
         //will load remote image into memory and then display image in imageview
 
         
@@ -152,12 +187,21 @@ class SatelliteViewController: UIViewController {
                 else if let d = data {
                     //print("loading image into the picture")
                     dispatch_async(dispatch_get_main_queue(), {
-                        self.datelabel.text = self.datestring!
-                        self.img.image = UIImage(data: d)
-                        self.img.contentMode = UIViewContentMode.ScaleAspectFit
+                        //self.datelabel.text = self.datestring!
+                        let curloc = locInfo(pic: UIImage(data: d)!, url: url, date: withDate)
+                        self.locHistory[currentCount] = curloc
+                        //self.locHistory.insert(curloc, atIndex:currentCount-1)
+                        self.counter--
+                        if self.counter == 0 {
+                            self.loadingComplete = false
+                            var timer = NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: Selector("displayImages"), userInfo: nil, repeats: true)
+
+                        }
+                        //self.img.contentMode = UIViewContentMode.ScaleAspectFit
                     })
                     
                 }
+                
                 
             })
             task.resume()
